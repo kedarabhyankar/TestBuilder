30f3ad0389f651280bf663d23d5497d75b3cdc3e
Kedar Abhyankar <krabhyankar@gmail.com>
2020-11-07 16:36:06 -0500

    Made changes to MethodTest.java and started work on method generator.

---
+++ src/MethodTest.java
@@ -0,0 +1,172 @@
+import java.util.Arrays;
+
+/**
+ * A specialized way to build a specific test case for a Method. Methods are then compared for equality
+ * by invocation of the {@code assertEquality} method.
+ */
+@SuppressWarnings("unused")
+public class MethodTest {
+
+    private String methodName;
+    private int[] modifiers;
+    private Class<?>[] throwsClazz;
+    private Class<?>[] parameterClazz;
+
+    /**
+     * The {@code MethodTest} constructor. In order to use this test builder, use this as a base and then build a
+     * test case out of it.
+     */
+    public MethodTest() {
+        //do nothing; this can't be used to make a test case. Build from here!
+    }
+
+    /**
+     * Set the expected method name
+     *
+     * @param methodName the method name to set
+     * @return the {@code MethodTest} with the newly built expected method name.
+     */
+    @SuppressWarnings("unused")
+    public MethodTest setMethodName(String methodName) {
+        this.methodName = methodName;
+        return this;
+    }
+
+    /**
+     * Adds an expected modifier to the {@code MethodTest}.
+     *
+     * @param modifierName the modifier to expect
+     * @return the {@code MethodTest} with the newly built modifier.
+     */
+    @SuppressWarnings("unused")
+    public MethodTest addModifier(String modifierName) {
+        if (this.modifiers == null) {
+            this.modifiers = new int[12];
+        }
+
+        return observeModifier(modifierName);
+    }
+
+    /**
+     * Adds an expected set of modifiers to the {@code MethodTest}
+     *
+     * @param modifierNames the modifiers to expect.
+     * @return the {@code MethodTest} with the newly built expected modifiers.
+     */
+    @SuppressWarnings("unused")
+    public MethodTest setModifiers(String[] modifierNames) {
+        this.modifiers = new int[12];
+        for (String s : modifierNames) {
+            //map the modifiers into int descriptors
+            observeModifier(s);
+        }
+        return this;
+    }
+
+    /**
+     * A method to add a throws clause to the expected method.
+     *
+     * @param throwClazz the class to throw. The class passed in must extend exception!
+     * @return the {@code MethodTest} with the updated throw clause.
+     */
+    @SuppressWarnings("unused")
+    public MethodTest addThrowsClause(Class<?> throwClazz) {
+        if (!throwClazz.getSuperclass().equals(Exception.class)) return this;
+        if (this.throwsClazz == null) {
+            this.throwsClazz = new Class<?>[1];
+        } else {
+            Class<?>[] temp = new Class<?>[this.throwsClazz.length];
+            System.arraycopy(throwsClazz, 0, temp, 0, throwsClazz.length);
+            this.throwsClazz = new Class<?>[temp.length + 1];
+            System.arraycopy(temp, 0, this.throwsClazz, 0, temp.length);
+            this.throwsClazz[temp.length] = throwClazz;
+        }
+
+        return this;
+    }
+
+
+    /**
+     * Add a parameter to this {@code MethodTest} object.
+     *
+     * @param paramClazz the parameter to add to the {@code MethodTest}.
+     * @return the {@code MethodTest} with the updated parameter list.
+     */
+    @SuppressWarnings("unused")
+    public MethodTest addParameter(Class<?> paramClazz) {
+        if (this.parameterClazz == null) {
+            this.parameterClazz = new Class<?>[1];
+            this.parameterClazz[0] = paramClazz;
+            return this;
+        }
+
+        Class<?>[] tempClazz = new Class<?>[this.parameterClazz.length];
+        //noinspection ManualArrayCopy
+        for (int i = 0; i < this.parameterClazz.length; i++) {
+            tempClazz[i] = this.parameterClazz[i];
+        }
+
+        this.parameterClazz = new Class<?>[this.parameterClazz.length + 1];
+        //noinspection ManualArrayCopy
+        for (int i = 0; i < tempClazz.length; i++) {
+            this.parameterClazz[i] = tempClazz[i];
+        }
+        this.parameterClazz[tempClazz.length] = paramClazz;
+        return this;
+    }
+
+    /**
+     * A method to check if two methods are equal. Invoke this from the expected object, and pass the
+     * generated MethodTest to this method to compare it for equality.
+     *
+     * @param methodTest the {@code MethodTest} to compare equality. Generally, this is the actual method
+     *                   to compare against
+     * @return true if equal, false if not.
+     */
+    @SuppressWarnings("unused")
+    public boolean assertEquality(MethodTest methodTest) {
+        return Arrays.equals(parameterClazz, methodTest.parameterClazz) && Arrays.equals(throwsClazz,
+                methodTest.throwsClazz) && this.methodName.equals(methodTest.methodName) &&
+                Arrays.equals(this.modifiers, methodTest.modifiers);
+    }
+
+
+    /**
+     * Updates the modifier array with the given state for its given name. Modifiers are organized in the
+     * array in the following fashion.
+     * <p>
+     * [0] -> public
+     * [1] -> private
+     * [2] -> protected
+     * [3] -> static
+     * [4] -> final
+     * [5] -> transient
+     * [6] -> abstract
+     * [7] -> native
+     * [8] -> interface
+     * [9] -> strict
+     * [10] -> synchronized
+     * [11] -> volatile
+     *
+     * @param modifierName The modifier name to set, in all lowercase.
+     * @return the {@code MethodTest} with the modified modifiers.
+     */
+    private MethodTest observeModifier(String modifierName) {
+        switch (modifierName) {
+            case "public" -> modifiers[0] = 1;
+            case "private" -> modifiers[1] = 1;
+            case "protected" -> modifiers[2] = 1;
+            case "static" -> modifiers[3] = 1;
+            case "final" -> modifiers[4] = 1;
+            case "transient" -> modifiers[5] = 1;
+            case "abstract" -> modifiers[6] = 1;
+            case "native" -> modifiers[7] = 1;
+            case "interface" -> modifiers[8] = 1;
+            case "strict" -> modifiers[9] = 1;
+            case "synchronized" -> modifiers[10] = 1;
+            case "volatile" -> modifiers[11] = 1;
+        }
+
+        return this;
+    }
+}
