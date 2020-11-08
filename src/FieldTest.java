c455bd57ac1adf941af15e3529ae478579d31006
Kedar Abhyankar <krabhyankar@gmail.com>
2020-11-08 12:24:42 -0500

    Create initial FieldTest.java class.

---
+++ src/FieldTest.java
@@ -0,0 +1,11 @@
+public class FieldTest {
+
+    private String fieldName;
+    private Class<?> fieldType;
+    private int[] modifiers;
+
+    public FieldTest() {
+        //do nothing
+    }
+
+}
